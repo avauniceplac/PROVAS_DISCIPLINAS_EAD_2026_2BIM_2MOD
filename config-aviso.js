011156163d721cbca2044e025323ab7b32f4f7c8
avauniceplac <118924628+avauniceplac@users.noreply.github.com>
2026-06-26 22:28:43 -0300

    Update config-aviso.js
    .

--- config-aviso.js
+++ config-aviso.js
@@ -6,7 +6,7 @@ const AVISO = {
   regulares: {
     ativo:    "on",
     titulo:   "Atenção",
-    mensagem: "Últimos dias para realizar suas provas!",
+    mensagem: "Último dia!",
   },
 
   /* ===== 2ª CHAMADA (segunda-chamada.html) ===== */
